913a71eaa2d15c9ce61be4c60cbd2083c8ea5830
Raghu Betina <raghu@firstdraft.com>
2018-06-15 04:59:36 +0000

    Generate owners resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Owner resource:
+  # CREATE
+  get "/owners/new", :controller => "owners", :action => "new"
+  post "/create_owner", :controller => "owners", :action => "create"
+
+  # READ
+  get "/owners", :controller => "owners", :action => "index"
+  get "/owners/:id", :controller => "owners", :action => "show"
+
+  # UPDATE
+  get "/owners/:id/edit", :controller => "owners", :action => "edit"
+  post "/update_owner/:id", :controller => "owners", :action => "update"
+
+  # DELETE
+  get "/delete_owner/:id", :controller => "owners", :action => "destroy"
+  #------------------------------
+
   devise_for :members
   # Routes for the Member resource:
   # READ
